584b9e58c9611595588825a002dfd331928571d6
GOODDEMOMAN <gooddemoman1987@gmail.com>
2026-03-18 19:01:06 +0300

    Quartz sync: Mar 18, 2026, 7:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: ""🧊 3D Wiki"",
+    pageTitle: ""3D Wiki"",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
